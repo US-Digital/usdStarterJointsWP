f3f3f9fb18cddd90f4c5934284b3f8db64b07ce5
USdigital <usdigital@andys-imac-2.unsworthsugden.local>
2020-02-07 17:23:49 +0000

    Removed bloat - opt in to features

--- assets/scripts/js/parallax.js
+++ assets/scripts/js/parallax.js
@@ -1,7 +1,10 @@
-window.addEventListener('scroll', function(e) {
-  var scrolled = window.pageYOffset
-  const background = document.querySelector('.parallax-container')
-  const output = 'calc(50% - ' + scrolled * 0.25 + 'px)'
-  console.log(output)
-  background.style.backgroundPositionY = output
-})
+const background = document.querySelector('.parallax-container')
+
+if (background) {
+  window.addEventListener('scroll', function(e) {
+    var scrolled = window.pageYOffset
+    const output = 'calc(50% - ' + scrolled * 0.25 + 'px)'
+    console.log(output)
+    background.style.backgroundPositionY = output
+  })
+}
