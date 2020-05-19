c8d122ce02b3138d633dbbb2fc3cb2c962b747b6
USdigital <usdigital@Andys-iMac-2.local>
2020-05-19 11:05:37 +0100

    Added Gutenberg dequeue

--- functions/enqueue-scripts.php
+++ functions/enqueue-scripts.php
@@ -2,18 +2,24 @@
 function site_scripts() {
   global $wp_styles; // Call global $wp_styles variable to add conditional wrapper around ie stylesheet the WordPress way
         
-    // Adding scripts file in the footer
-    wp_enqueue_script( 'site-js', get_template_directory_uri() . '/assets/scripts/scripts.js#asyncload', array( 'jquery' ), filemtime(get_template_directory() . '/assets/scripts/scripts.js'), true );
-   
-    // Register main stylesheet
-    wp_enqueue_style( 'site-css', get_template_directory_uri() . '/assets/styles/style.css', array(), filemtime(get_template_directory() . '/assets/styles/style.css'), 'all' );
-    
-    // Register slick slider default CSS
-    wp_enqueue_style( 'slick-css', get_template_directory_uri() . '/assets/styles/slick.css', array(), filemtime(get_template_directory() . '/assets/styles/slick.css'), 'all' );
+  // Adding scripts file in the footer
+  wp_enqueue_script( 'site-js', get_template_directory_uri() . '/assets/scripts/scripts.js#asyncload', array( 'jquery' ), filemtime(get_template_directory() . '/assets/scripts/scripts.js'), true );
+  
+  // Register main stylesheet
+  wp_enqueue_style( 'site-css', get_template_directory_uri() . '/assets/styles/style.css', array(), filemtime(get_template_directory() . '/assets/styles/style.css'), 'all' );
+  
+  // Register slick slider default CSS
+  wp_enqueue_style( 'slick-css', get_template_directory_uri() . '/assets/styles/slick.css', array(), filemtime(get_template_directory() . '/assets/styles/slick.css'), 'all' );
 
-    // Comment reply script for threaded comments
-    if ( is_singular() AND comments_open() AND (get_option('thread_comments') == 1)) {
-      wp_enqueue_script( 'comment-reply' );
-    }
+  // Comment reply script for threaded comments
+  if ( is_singular() AND comments_open() AND (get_option('thread_comments') == 1)) {
+    wp_enqueue_script( 'comment-reply' );
+  }
+
+  // Dequeue Gutenberg scripts
+  if( !function_exists( 'is_gutenberg_page' ) ) {
+    wp_dequeue_style('wp-block-library');
+    wp_dequeue_style('wp-block-library-theme');
+  }
 }
 add_action('wp_enqueue_scripts', 'site_scripts', 999);
